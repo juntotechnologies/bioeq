04018e46ced66fce7240c42922d2bc803190e836
Shaun Porwal <shaun.porwal@gmail.com>
2025-01-27 01:25:51 -0500

    Update init

--- bioeq/__init__.py
+++ bioeq/__init__.py
@@ -6,8 +6,10 @@ from importlib import metadata
 
 __version__ = metadata.version("bioeq")
 
-# Move these imports to module level
-from .bioeq import BioEq
-from .crossover2x2 import Crossover2x2
+try:
+    from .bioeq import BioEq
+    from .crossover2x2 import Crossover2x2
 
-__all__ = ["BioEq", "Crossover2x2"]
+    __all__ = ["BioEq", "Crossover2x2"]
+except ImportError:
+    pass
