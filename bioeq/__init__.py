11341bd0a88882e7182882ec9688287dbfeb4971
Shaun Porwal <shaun.porwal@gmail.com>
2025-01-26 20:13:58 -0500

    Update init to get version from inbuilt system

--- bioeq/__init__.py
+++ bioeq/__init__.py
@@ -2,35 +2,18 @@
 __init__ file for library
 """
 
-import tomli  # TOML parser for Python <3.11
-from pathlib import Path
+from importlib import metadata
 
 from .bioeq import BioEq
 from .crossover2x2 import Crossover2x2
 
 __all__ = ["BioEq", "Crossover2x2"]
 
-
-# Dynamically extract version from pyproject.toml
-def get_version():
-    """
-    Reads the version of the package from pyproject.toml.
-
-    Returns:
-        str: The version specified in the pyproject.toml file.
-    """
-    pyproject_path = Path(__file__).parent.parent / "pyproject.toml"
-    with pyproject_path.open("rb") as f:
-        pyproject_data = tomli.load(f)
-    return pyproject_data["tool"]["poetry"]["version"]
+__version__ = metadata.version("bioeq")
 
 
-# test function
 def hello() -> str:
     """
     Initial hello function
     """
     return "Hello from bioeq! This is a test for 0.1.0.1"
-
-
-__version__ = get_version()
