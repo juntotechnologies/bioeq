a2a213e843b302191e610616489e62b78962b182
Shaun Porwal <shaun.porwal@gmail.com>
2025-01-27 20:56:32 -0500

    Remove bioeq module, just use crossover2x2

--- bioeq/__init__.py
+++ bioeq/__init__.py
@@ -10,7 +10,4 @@ except metadata.PackageNotFoundError:
     __version__ = "unknown"
 
 # Direct imports
-from .bioeq import BioEq
 from .crossover2x2 import Crossover2x2
-
-__all__ = ["BioEq", "Crossover2x2"]
