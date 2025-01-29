8eea0519aae1a540360b4806eead6f51d9c81b6a
Shaun Porwal <shaun.porwal@gmail.com>
2025-01-29 00:07:15 -0500

    Add missing param test

--- tests/test_crossover2x2_validations.py
+++ tests/test_crossover2x2_validations.py
@@ -1,5 +1,6 @@
 import pytest
 import polars as pl
+import re
 from bioeq.crossover2x2 import Crossover2x2
 
 
@@ -10,11 +11,13 @@ def simdata1():
     )
 
 
-def test_validations(simdata1):
+def test_column_not_found_error(simdata1):
 
     with pytest.raises(
         ValueError,
-        match="Required column(s) not found in dataset: period, Concentration (bg/mL)",
+        match=re.escape(  # re.escape escapes all regex special chars in string, in this case parentheses and slashes
+            "Required column(s) not found in dataset: period, Concentration (bg/mL)"
+        ),
     ):
         Crossover2x2(
             data=simdata1,
@@ -25,3 +28,14 @@ def test_validations(simdata1):
             subject_col="SubjectID",
             form_col="Formulation",
         )
+
+
+def test_missing_param_value(simdata1):
+
+    with pytest.raises(
+        TypeError,
+        match=re.escape(
+            "Crossover2x2.__init__() missing 7 required positional arguments: 'data', 'subject_col', 'seq_col', 'period_col', 'time_col', 'conc_col', and 'form_col'"
+        ),
+    ):
+        Crossover2x2()
